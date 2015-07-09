ad118a083b0a6dadedd579c7abd4cc282fb42c1d
David Schlachter <david+gh@schlachter.ca>
2015-07-09 06:45:46 -0400

    Create Xcode project

---
+++ fast-xattr-test/fast-xattr-test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  fast-xattr-test
+//
+//  Created by David Schlachter on 2015-07-09.
+//  Copyright (c) 2015 David Schlachter. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
